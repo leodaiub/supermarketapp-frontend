3d335d3414d50e2375dfdfcd9ca2f47ef9e0321c
leodaiub <leodaiub@gmail.com>
2019-09-05 02:08:41 -0300

    almost finishing

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,9 +1,114 @@
-import React from 'react';
+import React, { Component } from 'react';
+import './Create.css';
+import api from '../services/api';
 
-function Create(){
-    return (
-        <div>asdasdasdsdadsd</div>
-    );
+export default class Create extends Component {
+    constructor(props){
+        super(props)
+        this.state ={
+            superMarketLocation: {
+              street: '',
+              number: '',
+              district: '',
+              zip: '',
+              country: '',
+              city: '',
+              state: ''
+            },
+            superMarketName: '',
+            superMarketPhone: '',
+            superMarketDescription: '',
+            superMarketAdditionalImages: null,
+        };
+        this.handlgeImageChange = this.handlgeImageChange.bind(this)
+    }
+   
+
+    handleSubmit = async e => {
+        e.preventDefault();
+        const data = new FormData();
+        const files = Array.from(this.state.superMarketAdditionalImages);
+
+        data.append("superMarketAdditionalImages", files[0]);
+        data.append("superMarketAdditionalImages", files[1]);
+        data.append("superMarketAdditionalImages", files[2]);
+        data.append("superMarketAdditionalImages", files[3]);
+        data.append('superMarketName', this.state.superMarketName);
+        data.append('superMarketPhone', this.state.superMarketPhone);
+        data.append('superMarketDescription', this.state.superMarketDescription);
+        data.append('superMarketLocation', JSON.stringify(this.state.superMarketLocation));
+
+        await api.post('market', data);
+
+        this.props.history.push('/');
+    }
+
+    handlgeImageChange = e => {
+        let files = e.target.files;
+        this.setState({ superMarketAdditionalImages: files }, () => { console.log(this.state.superMarketAdditionalImages) });
+    }
+
+    handleChange = e => {
+        this.setState({ [e.target.name]: e.target.value });
+        console.log(this.state)
+     }
+
+     handleChangeObject = e => {
+        this.setState({  superMarketLocation:{
+            ... this.state.superMarketLocation,
+            [e.target.name]: e.target.value }});
+        console.log(this.state)
+     }
+
+    render(){
+        return(
+            <form id="new-post" onSubmit={this.handleSubmit}>
+                <input type="file" onChange={this.handlgeImageChange} multiple="multiple"/>
+                <input 
+                    type="text" 
+                    name="superMarketName"
+                    placeholder="Nome"
+                    onChange={this.handleChange}
+                 />
+                <input 
+                    type="number" 
+                    name="superMarketPhone"
+                    placeholder="Telefone"
+                    onChange={this.handleChange}
+                />
+                <input 
+                    type="text" 
+                    name="superMarketDescription"
+                    placeholder="Descrição"
+                    onChange={this.handleChange}
+                  
+                 />
+                <input 
+                    type="text" 
+                    name="street"
+                    placeholder="Rua"
+                    onChange={this.handleChangeObject}  
+                />
+                <input 
+                    type="number" 
+                    name="zip"
+                    placeholder="CEP"
+                    onChange={this.handleChangeObject}
+                />
+                <input 
+                    type="text" 
+                    name="city"
+                    placeholder="Cidade"
+                    onChange={this.handleChangeObject}
+                />
+                <input 
+                    type="text" 
+                    name="state"
+                    placeholder="Estado"
+                    onChange={this.handleChangeObject}
+                />
+                <button type="submit">Enviar</button>
+            </form>
+        );
+    }
 }
-
-export default Create;
--- src/pages/Record.js
+++ src/pages/Record.js
@@ -1,10 +1,139 @@
-import React, {useState} from 'react';
+import React, { Component } from 'react';
+import api from '../services/api';
+import './Record.css';
 
-export default function Record(props) {
-    
-    console.log('po'+useState);
+export default class Record extends Component {
+    state = {};
 
-    return (
-        <div>props._</div>
-    );
+    async componentDidMount() {
+       const id = this.props.match.params.id;
+
+       const response = await api.get(`/market/${id}/`);
+
+       await this.setState(response.data);
+    }
+
+    handleChange(event) {
+        this.setState({inputVal: event.target.value});
+      }
+
+    handleDelete = async e => {
+        e.preventDefault();
+
+        const id = this.props.match.params.id;
+
+        await api.delete(`/market/${id}/`);
+
+        this.props.history.push('/');
+    }
+
+    handleSubmit = async e => {
+        e.preventDefault();
+
+        const data = new FormData();
+
+        const id = this.props.match.params.id;
+
+        data.append('superMarketName', this.state.superMarketName);
+        data.append('superMarketPhone', this.state.superMarketPhone);
+        data.append('superMarketDescription', this.state.superMarketDescription);
+        data.append('superMarketLocation.Street', this.state.superMarketLocation.Street);
+        data.append('superMarketLocation.Zip', this.state.superMarketLocation.Zip);
+        data.append('superMarketLocation.city', this.state.superMarketLocation.city);
+        data.append('superMarketLocation.state', this.state.superMarketLocation.state);
+
+        console.log(data + "teste")
+        await api.put(`/market/${id}/`, data);
+
+        this.props.history.push('/');
+    }
+
+    handleChange = e => {
+        e.preventDefault();
+        this.setState({ [e.target.name]: e.target.value });
+    }
+
+    handlgeImageChange = e => {
+        this.setState({superMarketAdditionalImages: e.target.files});
+    }
+
+
+    render(){
+        return (
+            <div className="data-wrap">
+                <div className="images-wrap">
+                <div className="main-image">
+                <div><img src={this.state.superMarketMainImage ? this.state.superMarketMainImage.location : ''} alt=""/></div><input type="file" onChange={this.handlgeImageChange}/>
+                </div>
+                <div className="additional-images">
+                    <div><img src={this.state.superMarketMainImage ? this.state.superMarketMainImage.location : ''} alt=""/><input type="file" onChange={this.handlgeImageChange}/></div>
+                    <div><img src={this.state.superMarketMainImage ? this.state.superMarketMainImage.location : ''} alt=""/><input type="file" onChange={this.handlgeImageChange}/></div>
+                    <div><img src={this.state.superMarketMainImage ? this.state.superMarketMainImage.location : ''} alt=""/><input type="file" onChange={this.handlgeImageChange}/></div>
+                </div>
+                </div>
+                <div className="form">
+                    <label>Nome:</label>
+                    <input 
+                        name="superMarketName"
+                        type="text"
+                        onChange={this.handleChange} 
+                        defaultValue={this.state.superMarketName} 
+                    />
+                    <label>Telefone:</label>
+                    <input
+                        name="superMarketPhone"
+                        type="text"
+                        onChange={this.handleChange} 
+                        defaultValue={this.state.superMarketPhone} 
+                    />
+                    <label>Descrição:</label>
+                    <input 
+                        name="superMarketDescription"
+                        type="text"
+                        onChange={this.handleChange} 
+                        defaultValue={this.state.superMarketDescription} 
+                    />
+                    <label>Rua:</label>
+                    <input 
+                        name="superMarketLocation.Street"
+                        type="text"
+                        onChange={this.handleChange} 
+                        defaultValue={this.state.superMarketLocation ? this.state.superMarketLocation.Street : ''} 
+                    />
+                    <label>CEP:</label>
+                    <input 
+                        name="superMarketLocation.Zip"
+                        type="text"
+                        onChange={this.handleChange} 
+                        defaultValue={this.state.superMarketLocation ? this.state.superMarketLocation.Zip : ''} 
+                    />
+                    <label>Cidade:</label>
+                    <input
+                        name="superMarketLocation.city"
+                        type="text"
+                        onChange={this.handleChange} 
+                        defaultValue={this.state.superMarketLocation ? this.state.superMarketLocation.city : ''} 
+                    />
+                    <label>Estado:</label>
+                    <input 
+                        name="superMarketLocation.state"
+                        type="text"
+                        onChange={this.handleChange} 
+                        defaultValue={this.state.superMarketLocation ? this.state.superMarketLocation.state : ''} 
+                    />
+                    <input 
+                        type="button" 
+                        value="Send"
+                        onClick={this.handleSubmit}
+                    />
+                    <input 
+                        type="button" 
+                        value="DELETE"
+                        onClick={this.handleDelete}
+                    />
+                    {console.log(this.state)}
+                </div>
+            </div> 
+        );
+    }
 }
