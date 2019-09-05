78b5e8c08b30870a4debecbd4b93e418e47535d1
leodaiub <leodaiub@gmail.com>
2019-09-05 14:15:45 -0300

    app finished

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -27,7 +27,11 @@ export default class Create extends Component {
     handleSubmit = async e => {
         e.preventDefault();
         const data = new FormData();
-        const files = Array.from(this.state.superMarketAdditionalImages);
+        const files = [];
+        files.push(this.state.superMarketAdditionalImages[0]);
+        files.push(this.state.superMarketAdditionalImages[1]);
+        files.push(this.state.superMarketAdditionalImages[2]);
+        files.push(this.state.superMarketAdditionalImages[3]);
 
         data.append("superMarketAdditionalImages", files[0]);
         data.append("superMarketAdditionalImages", files[1]);
@@ -39,72 +43,97 @@ export default class Create extends Component {
         data.append('superMarketLocation', JSON.stringify(this.state.superMarketLocation));
 
         await api.post('market', data);
-        console.log(JSON.stringify(Object.fromEntries(data)));
         this.props.history.push('/');
     }
 
     handlgeImageChange = e => {
-        let files = e.target.files;
-        this.setState({ superMarketAdditionalImages: files }, () => { console.log(this.state.superMarketAdditionalImages) });
+        this.setState({ superMarketAdditionalImages: e.target.files }, () => { console.log(this.state.superMarketAdditionalImages) });
     }
 
     handleChange = e => {
         this.setState({ [e.target.name]: e.target.value });
-        console.log(this.state)
      }
 
      handleChangeObject = e => {
         this.setState({  superMarketLocation:{
-            ... this.state.superMarketLocation,
+            ...this.state.superMarketLocation,
             [e.target.name]: e.target.value }});
-        console.log(this.state)
      }
 
     render(){
         return(
             <form id="new-post" onSubmit={this.handleSubmit}>
-                <input type="file" onChange={this.handlgeImageChange} multiple="multiple"/>
+                <label>Images: (max of four)</label>
+                <input type="file"  required onChange={this.handlgeImageChange} multiple="multiple"/>
                 <input 
                     type="text" 
                     name="superMarketName"
-                    placeholder="Nome"
+                    placeholder="Name"
+                    required
                     onChange={this.handleChange}
                  />
                 <input 
                     type="number" 
                     name="superMarketPhone"
-                    placeholder="Telefone"
+                    placeholder="Phone"
+                    required
                     onChange={this.handleChange}
                 />
                 <input 
                     type="text" 
                     name="superMarketDescription"
-                    placeholder="Descrição"
+                    placeholder="Description"
+                    required
                     onChange={this.handleChange}
                   
                  />
                 <input 
                     type="text" 
                     name="street"
-                    placeholder="Rua"
+                    placeholder="Street"
+                    required
+                    onChange={this.handleChangeObject}  
+                />
+                <input 
+                    type="text" 
+                    name="number"
+                    placeholder="Number"
+                    required
+                    onChange={this.handleChangeObject}  
+                />
+                <input 
+                    type="text" 
+                    name="district"
+                    placeholder="District"
+                    required
                     onChange={this.handleChangeObject}  
                 />
                 <input 
                     type="number" 
                     name="zip"
-                    placeholder="CEP"
+                    placeholder="Zip Code"
+                    required
                     onChange={this.handleChangeObject}
                 />
                 <input 
                     type="text" 
                     name="city"
-                    placeholder="Cidade"
+                    placeholder="City"
+                    required
                     onChange={this.handleChangeObject}
                 />
                 <input 
                     type="text" 
                     name="state"
-                    placeholder="Estado"
+                    placeholder="State"
+                    required
+                    onChange={this.handleChangeObject}
+                />
+                 <input 
+                    type="text" 
+                    name="country"
+                    placeholder="Country"
+                    required
                     onChange={this.handleChangeObject}
                 />
                 <button type="submit">Enviar</button>
--- src/pages/List.js
+++ src/pages/List.js
@@ -1,8 +1,6 @@
 import React, { Component} from 'react';
 import './List.css';
 import api from '../services/api';
-//import io from 'socket.io-client';
-import cart from '../assets/shopping-cart.svg';
 import {Link }from 'react-router-dom';
 
 export default class List extends Component {
@@ -11,12 +9,10 @@ export default class List extends Component {
   };
 
   async componentDidMount() {
-      //this.registerToSocket();
 
       const response = await api.get();
 
       this.setState({list: response.data});
-
   }
   
 
@@ -28,28 +24,31 @@ export default class List extends Component {
             <div className="item-wrapper">
               <div className='image'>
                 <img 
-                src={market.superMarketMainImage ? market.superMarketMainImage.location : cart} 
-                width ='200' alt=""/>
+                src={market.superMarketMainImage ? market.superMarketMainImage.location : market.superMarketMainImage.location} 
+                width ='200' height='150' alt=""/>
               </div>
               <div className='images'>
                 <img 
-                src={market.superMarketMainImage ? market.superMarketMainImage.location : cart} 
+                src={market.superMarketAdditionalImages ? market.superMarketAdditionalImages[0][0].location : market.superMarketMainImage.location} 
                 width ='100' alt=""/>
                 <img 
-                src={market.superMarketMainImage ? market.superMarketMainImage.location : cart} 
+                src={market.superMarketAdditionalImages ? market.superMarketAdditionalImages[1][0].location : market.superMarketMainImage.location} 
                 width ='100' alt=""/>
                 <img 
-                src={market.superMarketMainImage ? market.superMarketMainImage.location : cart} 
+                src={market.superMarketAdditionalImages ? market.superMarketAdditionalImages[2][0].location : market.superMarketMainImage.location} 
                 width ='100' alt=""/>
               </div>
               <div className="data">
-                <p><strong>Nome:</strong> { market.superMarketName ? market.superMarketName : 'ola' }</p>
-                <p><strong>Telefone:</strong> { market.superMarketName ? market.superMarketPhone : 'ola' }</p>
-                <p><strong>Descrição:</strong> { market.superMarketName ? market.superMarketDescription : 'ola' }</p>
-                <p><strong>Rua:</strong> { market.superMarketLocation ? market.superMarketLocation.Street : 'ola' }</p>
-                <p><strong>CEP:</strong> { market.superMarketLocation ? market.superMarketLocation.Zip : 'ola' }</p>
-                <p><strong>Cidade:</strong> { market.superMarketLocation ? market.superMarketLocation.city : 'ola' }</p>
-                <p><strong>Estado:</strong> {market.superMarketLocation ? market.superMarketLocation.state : 'ola' }</p>
+                <p><strong>Name:</strong> { market.superMarketName ? market.superMarketName : 'ola' }</p>
+                <p><strong>Phone:</strong> { market.superMarketName ? market.superMarketPhone : 'ola' }</p>
+                <p><strong>Description:</strong> { market.superMarketName ? market.superMarketDescription : 'ola' }</p>
+                <p><strong>Street:</strong> { market.superMarketLocation ? market.superMarketLocation.street : 'ola' }</p>
+                <p><strong>Number:</strong> { market.superMarketLocation ? market.superMarketLocation.number : 'ola' }</p>
+                <p><strong>Zip Code:</strong> { market.superMarketLocation ? market.superMarketLocation.zip : 'ola' }</p>
+                <p><strong>District:</strong> { market.superMarketLocation ? market.superMarketLocation.district : 'ola' }</p>
+                <p><strong>City:</strong> { market.superMarketLocation ? market.superMarketLocation.city : 'ola' }</p>
+                <p><strong>State:</strong> { market.superMarketLocation ? market.superMarketLocation.state : 'ola' }</p>
+                <p><strong>Country:</strong> {market.superMarketLocation ? market.superMarketLocation.country : 'ola' }</p>
               </div>   
             </div>
             </Link>
