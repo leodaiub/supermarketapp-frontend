7c6d109b44de5a9f3c2a355e5f50a0650b169081
leodaiub <leodaiub@gmail.com>
2019-09-06 16:56:44 -0300

    adjustments

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,25 +5,27 @@ import './Header.css';
 
 export default function Header(){
     return (
-      <div className='header-wrap'>
-        <div className="button">
-          <Link to="/">
-          <button className="button-item">Home</button>
-          </Link>
-        </div>
-        <Link className="logo" to="/"> 
-        <div className="logo">     
-          <div className='logo-img'>
-              <img src={logo} alt="Shopping Cart"/>
+      <header>
+        <div className='header-wrap'>
+          <div className="button">
+            <Link to="/">
+            <button className="button-item">Home</button>
+            </Link>
           </div>
-          <div className="logo-text">SuperMarketApp</div>
+          <Link className="logo" to="/"> 
+          <div className="logo">     
+            <div className='logo-img'>
+                <img src={logo} alt="Shopping Cart"/>
+            </div>
+            <div className="logo-text">SuperMarketApp</div>
+          </div>
+          </Link>  
+          <div className="button">
+          <Link to="/Create/">
+            <button className="button-item">New</button>
+          </Link>
+          </div>
         </div>
-        </Link>  
-        <div className="button">
-        <Link to="/Create/">
-          <button className="button-item">New</button>
-        </Link>
-        </div>
-      </div> 
+      </header>      
     );
 }
