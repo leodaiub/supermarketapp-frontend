1fea087eb0e1f383afc002b682a3e5919165cd90
leodaiub <leodaiub@gmail.com>
2019-09-06 11:56:19 -0300

    deploy

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3030/',
+    baseURL: 'http://supermarketappapi-dev.us-west-2.elasticbeanstalk.com/',
 });
 
 export default api;
